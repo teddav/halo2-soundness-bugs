1b3eb35ae05a866a2812a52ee109cb5ed55bc81b
teddav <teddav@users.noreply.github.com>
2025-02-28 22:24:18 +0100

    wrote merkle circuit with poseidon

--- src/main.rs
+++ src/main.rs
@@ -38,6 +38,9 @@ use non_deterministic::NonDeterministicCircuit;
 mod hash;
 use hash::HashCircuit;
 
+mod merkle;
+use merkle::MerkleCircuit;
+
 // fn main() {
 //     let circuit = CasinoCircuit {
 //         deposits: vec![
@@ -87,22 +90,53 @@ use hash::HashCircuit;
 //     assert!(prover.verify().is_ok());
 // }
 
+// fn main() {
+//     let rng = OsRng;
+
+//     // let message = [PallasFp::random(rng), PallasFp::random(rng)];
+//     let message = [PallasFp::from(1), PallasFp::from(2)];
+//     println!("message: {:#?}", message);
+//     let output = Hash::<_, OrchardNullifier, ConstantLength<2>, 3, 2>::init().hash(message);
+//     println!("output: {:#?}", output);
+
+//     let circuit = HashCircuit::<OrchardNullifier, 3, 2, 2> {
+//         message: Value::known(message),
+//         output: Value::known(output),
+//         _spec: PhantomData,
+//     };
+
+//     let prover = MockProver::run(6, &circuit, vec![]).unwrap();
+//     // println!("prover: {:#?}", prover);
+//     assert!(prover.verify().is_ok());
+// }
+
 fn main() {
     let rng = OsRng;
 
     // let message = [PallasFp::random(rng), PallasFp::random(rng)];
-    let message = [PallasFp::from(1), PallasFp::from(2)];
-    println!("message: {:#?}", message);
-    let output = Hash::<_, OrchardNullifier, ConstantLength<2>, 3, 2>::init().hash(message);
-    println!("output: {:#?}", output);
+    let leaves = [
+        PallasFp::from(1),
+        PallasFp::from(2),
+        PallasFp::from(3),
+        PallasFp::from(4),
+    ];
+    let h1 =
+        Hash::<_, OrchardNullifier, ConstantLength<2>, 3, 2>::init().hash([leaves[0], leaves[1]]);
+    let h2 =
+        Hash::<_, OrchardNullifier, ConstantLength<2>, 3, 2>::init().hash([leaves[2], leaves[3]]);
+    let root = Hash::<_, OrchardNullifier, ConstantLength<2>, 3, 2>::init().hash([h1, h2]);
+    println!("root: {root:?}");
 
-    let circuit = HashCircuit::<OrchardNullifier, 3, 2, 2> {
-        message: Value::known(message),
-        output: Value::known(output),
+    let circuit = MerkleCircuit::<OrchardNullifier, 3, 2> {
+        leaf: Value::known(leaves[0]),
+        path_elements: vec![Value::known(leaves[1]), Value::known(h2)],
+        path_indices: vec![
+            Value::known(PallasFp::from(0)),
+            Value::known(PallasFp::from(0)),
+        ],
         _spec: PhantomData,
     };
 
-    let prover = MockProver::run(6, &circuit, vec![]).unwrap();
-    // println!("prover: {:#?}", prover);
+    let prover = MockProver::run(10, &circuit, vec![]).unwrap();
     assert!(prover.verify().is_ok());
 }
